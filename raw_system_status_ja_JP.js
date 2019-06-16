b307774d50216f4b11284bcd2f7689a3c698aa32
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2019-06-16 17:15:09 +0900

    diff commit 2019/06/16_17:15

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -59,7 +59,7 @@
           "epochEndDate": 1560622200000,
           "messageId": "1003000",
           "statusType": "Maintenance",
-          "datePosted": "2019/06/16 07:29 JST",
+          "datePosted": "2019/06/16 17:00 JST",
           "startDate": "2019/06/16 01:00 JST",
           "endDate": "2019/06/16 03:10 JST",
           "affectedServices": [
@@ -96,7 +96,7 @@
           "epochEndDate": 1560622200000,
           "messageId": "1003000",
           "statusType": "Maintenance",
-          "datePosted": "2019/06/16 07:29 JST",
+          "datePosted": "2019/06/16 17:00 JST",
           "startDate": "2019/06/16 01:00 JST",
           "endDate": "2019/06/16 03:10 JST",
           "affectedServices": [
@@ -332,24 +332,11 @@
       "events": [
         {
           "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1560363600000,
-          "epochEndDate": 1560370620000,
-          "messageId": "1002997",
-          "statusType": "Issue",
-          "datePosted": "2019/06/16 07:29 JST",
-          "startDate": "2019/06/13 03:20 JST",
-          "endDate": "2019/06/13 05:17 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "このサービスに関する問題が発生しました。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1560616200000,
           "epochEndDate": 1560622500000,
           "messageId": "2000357",
           "statusType": "Maintenance",
-          "datePosted": "2019/06/16 07:29 JST",
+          "datePosted": "2019/06/16 17:00 JST",
           "startDate": "2019/06/16 01:30 JST",
           "endDate": "2019/06/16 03:15 JST",
           "affectedServices": null,
