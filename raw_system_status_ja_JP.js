5f3697a7012b1fd742bb5efb264700d21fc6ff6d
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2021-03-19 03:06:23 +0900

    diff commit 2021/03/19_03:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -68,7 +68,7 @@
           "epochEndDate": 1616335200000,
           "messageId": "2000000464",
           "statusType": "Maintenance",
-          "datePosted": "2021/03/18 18:08 JST",
+          "datePosted": "2021/03/19 03:00 JST",
           "startDate": "2021/03/21 03:00 JST",
           "endDate": "2021/03/21 23:00 JST",
           "affectedServices": null,
@@ -164,7 +164,7 @@
           "epochEndDate": 1615910400000,
           "messageId": "2000000462",
           "statusType": "Issue",
-          "datePosted": "2021/03/18 18:08 JST",
+          "datePosted": "2021/03/19 03:00 JST",
           "startDate": "2021/03/16 21:45 JST",
           "endDate": "2021/03/17 01:00 JST",
           "affectedServices": null,
@@ -387,7 +387,19 @@
       "redirectUrl": null,
       "serviceName": "株価",
       "events": [
-
+        {
+          "usersAffected": "一部のユーザに影響します",
+          "epochStartDate": 1616080200000,
+          "epochEndDate": null,
+          "messageId": "1000000242",
+          "statusType": "Issue",
+          "datePosted": "2021/03/19 03:00 JST",
+          "startDate": "2021/03/19 00:10 JST",
+          "endDate": null,
+          "affectedServices": null,
+          "eventStatus": "ongoing",
+          "message": "「株価」で問題が発生しています。"
+        }
       ]
     },
     {
