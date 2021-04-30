02d6f9701cadd4dd127f3324e6ef5f9e50d717f5
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2021-05-01 06:06:29 +0900

    diff commit 2021/05/01_06:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -12,7 +12,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -45,7 +45,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -78,7 +78,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -111,7 +111,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -151,7 +151,7 @@
           "epochEndDate": 1619724840000,
           "messageId": "2000000588",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/30 03:50 JST",
           "endDate": "2021/04/30 04:34 JST",
           "affectedServices": null,
@@ -254,7 +254,7 @@
           "epochEndDate": 1619737380000,
           "messageId": "2000000591",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/30 05:44 JST",
           "endDate": "2021/04/30 08:03 JST",
           "affectedServices": [
@@ -270,7 +270,7 @@
           "epochEndDate": 1619630400000,
           "messageId": "2000000582",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/28 23:30 JST",
           "endDate": "2021/04/29 02:20 JST",
           "affectedServices": null,
@@ -289,7 +289,7 @@
           "epochEndDate": 1619637660000,
           "messageId": "2000000585",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:27 JST",
           "endDate": "2021/04/29 04:21 JST",
           "affectedServices": null,
@@ -318,29 +318,29 @@
       "events": [
         {
           "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1619787120000,
-          "epochEndDate": 1619794080000,
-          "messageId": "1000000313",
-          "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
-          "startDate": "2021/04/30 21:52 JST",
-          "endDate": "2021/04/30 23:48 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "このサービスに関して一時的に問題が発生した可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1619722500000,
           "epochEndDate": 1619729100000,
           "messageId": "2000000590",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/30 03:55 JST",
           "endDate": "2021/04/30 05:45 JST",
           "affectedServices": null,
           "eventStatus": "resolved",
           "message": "メールの速度が低下、またはメールを利用できなかった可能性があります。メールの復旧後、受信メッセージの遅延が発生した可能性があります。"
+        },
+        {
+          "usersAffected": "一部のユーザに影響しました",
+          "epochStartDate": 1619787120000,
+          "epochEndDate": 1619794080000,
+          "messageId": "1000000313",
+          "statusType": "Issue",
+          "datePosted": "2021/05/01 05:52 JST",
+          "startDate": "2021/04/30 21:52 JST",
+          "endDate": "2021/04/30 23:48 JST",
+          "affectedServices": null,
+          "eventStatus": "resolved",
+          "message": "このサービスに関して一時的に問題が発生した可能性があります。"
         }
       ]
     },
@@ -396,7 +396,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -457,7 +457,7 @@
           "epochEndDate": 1619637780000,
           "messageId": "2000000584",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/29 03:53 JST",
           "endDate": "2021/04/29 04:23 JST",
           "affectedServices": [
@@ -574,7 +574,7 @@
           "epochEndDate": 1619736420000,
           "messageId": "2000000591",
           "statusType": "Issue",
-          "datePosted": "2021/05/01 02:48 JST",
+          "datePosted": "2021/05/01 05:52 JST",
           "startDate": "2021/04/30 05:44 JST",
           "endDate": "2021/04/30 08:03 JST",
           "affectedServices": [
