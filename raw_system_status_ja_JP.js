231bfa0b3bd01f647de454078ea30f9078f8e777
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2021-05-12 04:30:48 +0900

    diff commit 2021/05/12_04:30

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -12,7 +12,7 @@
           "epochEndDate": 1620725400000,
           "messageId": "2000000624",
           "statusType": "Outage",
-          "datePosted": "2021/05/12 04:06 JST",
+          "datePosted": "2021/05/12 04:22 JST",
           "startDate": "2021/05/11 17:01 JST",
           "endDate": "2021/05/11 18:30 JST",
           "affectedServices": [
@@ -62,7 +62,7 @@
           "epochEndDate": 1620759600000,
           "messageId": "2000000622",
           "statusType": "Maintenance",
-          "datePosted": "2021/05/12 04:06 JST",
+          "datePosted": "2021/05/12 04:22 JST",
           "startDate": "2021/05/12 01:00 JST",
           "endDate": "2021/05/12 04:00 JST",
           "affectedServices": null,
@@ -104,21 +104,21 @@
       "serviceName": "Apple School Manager",
       "events": [
         {
-          "usersAffected": "一部のユーザに影響します",
+          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1620756540000,
-          "epochEndDate": null,
+          "epochEndDate": 1620760380000,
           "messageId": "2000000628",
           "statusType": "Issue",
-          "datePosted": "2021/05/12 04:06 JST",
+          "datePosted": "2021/05/12 04:22 JST",
           "startDate": "2021/05/12 03:09 JST",
-          "endDate": null,
+          "endDate": "2021/05/12 04:13 JST",
           "affectedServices": [
             "Apple School Manager",
             "スクールワーク",
             null
           ],
-          "eventStatus": "ongoing",
-          "message": "This service may be slow or unavailable."
+          "eventStatus": "resolved",
+          "message": "This service may have been slow or unavailable."
         }
       ]
     },
@@ -293,7 +293,7 @@
           "epochEndDate": 1620725400000,
           "messageId": "2000000624",
           "statusType": "Outage",
-          "datePosted": "2021/05/12 04:06 JST",
+          "datePosted": "2021/05/12 04:22 JST",
           "startDate": "2021/05/11 17:01 JST",
           "endDate": "2021/05/11 18:30 JST",
           "affectedServices": [
@@ -380,21 +380,21 @@
       "serviceName": "スクールワーク",
       "events": [
         {
-          "usersAffected": "一部のユーザに影響します",
+          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1620756540000,
-          "epochEndDate": null,
+          "epochEndDate": 1620760380000,
           "messageId": "2000000628",
           "statusType": "Issue",
-          "datePosted": "2021/05/12 04:06 JST",
+          "datePosted": "2021/05/12 04:22 JST",
           "startDate": "2021/05/12 03:09 JST",
-          "endDate": null,
+          "endDate": "2021/05/12 04:13 JST",
           "affectedServices": [
             "Apple School Manager",
             "スクールワーク",
             null
           ],
-          "eventStatus": "ongoing",
-          "message": "このサービスの速度が低下する、またはこのサービスを利用できない可能性があります。"
+          "eventStatus": "resolved",
+          "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
         }
       ]
     },
