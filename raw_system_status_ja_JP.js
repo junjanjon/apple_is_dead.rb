57c4f273ef20caf37b15bcde267e402abc030909
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2021-10-27 17:06:22 +0900

    diff commit 2021/10/27_17:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -81,7 +81,7 @@
           "epochEndDate": 1635267180000,
           "messageId": "2000000939",
           "statusType": "Issue",
-          "datePosted": "2021/10/27 02:44 JST",
+          "datePosted": "2021/10/27 17:00 JST",
           "startDate": "2021/10/27 01:12 JST",
           "endDate": "2021/10/27 01:53 JST",
           "affectedServices": null,
