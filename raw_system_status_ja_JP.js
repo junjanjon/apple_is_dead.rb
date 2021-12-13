bb605e78de2eda4961c35dc3c291fccf57621d37
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2021-12-13 18:06:22 +0900

    diff commit 2021/12/13_18:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -68,55 +68,14 @@
     {
       "redirectUrl": null,
       "events": [
-        {
-          "usersAffected": "すべてのユーザに影響しました",
-          "epochStartDate": 1639151160000,
-          "epochEndDate": 1639155960000,
-          "messageId": "1000000844",
-          "statusType": "Outage",
-          "datePosted": "2021/12/13 10:38 JST",
-          "startDate": "2021/12/11 00:46 JST",
-          "endDate": "2021/12/11 02:06 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "Mastercardカード保有者は、Apple Payでカードの追加、停止、削除、または購入ができませんでした。"
-        }
+
       ],
       "serviceName": "Apple Pay & Wallet"
     },
     {
       "redirectUrl": null,
       "events": [
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1639099200000,
-          "epochEndDate": 1639107900000,
-          "messageId": "2000001034",
-          "statusType": "Issue",
-          "datePosted": "2021/12/13 10:38 JST",
-          "startDate": "2021/12/10 10:20 JST",
-          "endDate": "2021/12/10 19:10 JST",
-          "affectedServices": [
-            "Apple School Manager",
-            "iCloud Web Apps (iCloud.com)",
-            null
-          ],
-          "eventStatus": "resolved",
-          "message": "ユーザがサインインできなかった可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1639176000000,
-          "epochEndDate": 1639182240000,
-          "messageId": "2000001039",
-          "statusType": "Issue",
-          "datePosted": "2021/12/13 10:38 JST",
-          "startDate": "2021/12/11 07:40 JST",
-          "endDate": "2021/12/11 09:24 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "ユーザは Apple School Manager に登録できませんでした。"
-        }
+
       ],
       "serviceName": "Apple School Manager"
     },
@@ -200,36 +159,7 @@
     {
       "redirectUrl": null,
       "events": [
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1639099200000,
-          "epochEndDate": 1639131000000,
-          "messageId": "2000001034",
-          "statusType": "Issue",
-          "datePosted": "2021/12/13 10:38 JST",
-          "startDate": "2021/12/10 10:20 JST",
-          "endDate": "2021/12/10 19:10 JST",
-          "affectedServices": [
-            "Apple School Manager",
-            "iCloud Web Apps (iCloud.com)",
-            null
-          ],
-          "eventStatus": "resolved",
-          "message": "ユーザがicloud.comへサインインできなかった可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1639134000000,
-          "epochEndDate": 1639143600000,
-          "messageId": "1000000843",
-          "statusType": "Issue",
-          "datePosted": "2021/12/13 10:38 JST",
-          "startDate": "2021/12/10 20:00 JST",
-          "endDate": "2021/12/10 22:40 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "ユーザがicloud.comへサインインできなかった可能性があります。"
-        }
+
       ],
       "serviceName": "iCloud Web Apps (iCloud.com)"
     },
