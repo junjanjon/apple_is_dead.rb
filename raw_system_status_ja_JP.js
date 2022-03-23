cf0bb3f9775e7f14ab3d32401b20bd02135770b1
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-03-23 17:06:30 +0900

    diff commit 2022/03/23_17:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -74,7 +74,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001316",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:25 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -97,7 +97,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001314",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -113,7 +113,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -168,22 +168,9 @@
           "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1647880020000,
           "epochEndDate": 1647882900000,
-          "messageId": "2000001312",
-          "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
-          "startDate": "2022/03/22 01:27 JST",
-          "endDate": "2022/03/22 02:15 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "Device Enrollment Programにサインインできませんでした。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1647880020000,
-          "epochEndDate": 1647882900000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -206,6 +193,19 @@
           ],
           "eventStatus": "resolved",
           "message": "Device Enrollment Programにサインインできませんでした。"
+        },
+        {
+          "usersAffected": "一部のユーザに影響しました",
+          "epochStartDate": 1647880020000,
+          "epochEndDate": 1647882900000,
+          "messageId": "2000001312",
+          "statusType": "Outage",
+          "datePosted": "2022/03/23 07:49 JST",
+          "startDate": "2022/03/22 01:27 JST",
+          "endDate": "2022/03/22 02:15 JST",
+          "affectedServices": null,
+          "eventStatus": "resolved",
+          "message": "Device Enrollment Programにサインインできませんでした。"
         }
       ],
       "serviceName": "Device Enrollment Program"
@@ -219,7 +219,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001315",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": null,
@@ -245,7 +245,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -275,7 +275,7 @@
           "epochEndDate": 1647987060000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
@@ -319,7 +319,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -355,7 +355,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -385,7 +385,7 @@
           "epochEndDate": 1647987120000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
@@ -408,7 +408,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -444,7 +444,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -480,7 +480,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -537,7 +537,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -573,7 +573,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -623,7 +623,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -659,7 +659,7 @@
           "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -695,7 +695,7 @@
           "epochEndDate": 1647882840000,
           "messageId": "2000001300",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:25 JST",
           "endDate": "2022/03/22 02:14 JST",
           "affectedServices": null,
@@ -733,22 +733,9 @@
           "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1647880020000,
           "epochEndDate": 1647882780000,
-          "messageId": "2000001313",
-          "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
-          "startDate": "2022/03/22 01:27 JST",
-          "endDate": "2022/03/22 02:13 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1647880020000,
-          "epochEndDate": 1647882780000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -769,6 +756,19 @@
             null,
             null
           ],
+          "eventStatus": "resolved",
+          "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
+        },
+        {
+          "usersAffected": "一部のユーザに影響しました",
+          "epochStartDate": 1647880020000,
+          "epochEndDate": 1647882780000,
+          "messageId": "2000001313",
+          "statusType": "Outage",
+          "datePosted": "2022/03/23 07:49 JST",
+          "startDate": "2022/03/22 01:27 JST",
+          "endDate": "2022/03/22 02:13 JST",
+          "affectedServices": null,
           "eventStatus": "resolved",
           "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
         }
@@ -833,7 +833,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001314",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -849,7 +849,7 @@
           "epochEndDate": 1647882900000,
           "messageId": "2000001303",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:15 JST",
           "affectedServices": [
@@ -892,7 +892,7 @@
           "epochEndDate": 1647883800000,
           "messageId": "2000001301",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:32 JST",
           "endDate": "2022/03/22 02:30 JST",
           "affectedServices": null,
@@ -925,7 +925,7 @@
           "epochEndDate": 1647882840000,
           "messageId": "2000001299",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:14 JST",
           "affectedServices": [
@@ -950,7 +950,7 @@
           "epochEndDate": 1647882840000,
           "messageId": "2000001299",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:14 JST",
           "affectedServices": [
@@ -989,7 +989,7 @@
           "epochEndDate": 1647882840000,
           "messageId": "2000001299",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:14 JST",
           "affectedServices": [
@@ -1021,7 +1021,7 @@
           "epochEndDate": 1647987060000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
@@ -1044,7 +1044,7 @@
           "epochEndDate": 1647882840000,
           "messageId": "2000001299",
           "statusType": "Outage",
-          "datePosted": "2022/03/23 17:00 JST",
+          "datePosted": "2022/03/23 07:49 JST",
           "startDate": "2022/03/22 01:27 JST",
           "endDate": "2022/03/22 02:14 JST",
           "affectedServices": [
