f6b7f2089cf3a113a7179491441787a9b2995631
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-03-25 14:06:20 +0900

    diff commit 2022/03/25_14:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -1,6 +1,6 @@
 {
-  "drMessage": null,
   "drpost": false,
+  "drMessage": null,
   "services": [
     {
       "redirectUrl": null,
@@ -137,7 +137,7 @@
           "epochEndDate": 1647987060000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
@@ -188,7 +188,7 @@
           "epochEndDate": 1647987120000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
@@ -225,7 +225,7 @@
           "epochEndDate": 1648155540000,
           "messageId": "2000001322",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/25 05:27 JST",
           "endDate": "2022/03/25 07:55 JST",
           "affectedServices": [
@@ -303,7 +303,7 @@
           "epochEndDate": 1648162500000,
           "messageId": "2000001322",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/25 05:27 JST",
           "endDate": "2022/03/25 07:55 JST",
           "affectedServices": [
@@ -353,7 +353,7 @@
           "epochEndDate": 1648163160000,
           "messageId": "2000001323",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/25 06:50 JST",
           "endDate": "2022/03/25 08:06 JST",
           "affectedServices": null,
@@ -500,17 +500,17 @@
       "redirectUrl": null,
       "events": [
         {
-          "usersAffected": "一部のユーザに影響します",
+          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1648155600000,
-          "epochEndDate": null,
+          "epochEndDate": 1648182180000,
           "messageId": "2000001324",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/25 06:00 JST",
-          "endDate": null,
+          "endDate": "2022/03/25 13:23 JST",
           "affectedServices": null,
-          "eventStatus": "ongoing",
-          "message": "このサービスに関する問題が発生しています。現在、問題の原因を究明中です。新しい情報が入り次第、状況を更新します。"
+          "eventStatus": "resolved",
+          "message": "このサービスに関する問題が発生しました。"
         }
       ],
       "serviceName": "写真"
@@ -524,7 +524,7 @@
           "epochEndDate": 1647987060000,
           "messageId": "2000001319",
           "statusType": "Issue",
-          "datePosted": "2022/03/25 10:37 JST",
+          "datePosted": "2022/03/25 13:52 JST",
           "startDate": "2022/03/23 05:47 JST",
           "endDate": "2022/03/23 07:12 JST",
           "affectedServices": [
