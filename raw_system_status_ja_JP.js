b6c97801a79e1a090a24a245191c0e3ee067084a
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-05-14 13:30:45 +0900

    diff commit 2022/05/14_13:30

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -46,7 +46,7 @@
           "epochEndDate": 1652392440000,
           "messageId": "2000001429",
           "statusType": "Issue",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/13 03:42 JST",
           "endDate": "2022/05/13 06:54 JST",
           "affectedServices": [
@@ -84,7 +84,7 @@
           "epochEndDate": 1652392440000,
           "messageId": "2000001429",
           "statusType": "Issue",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/13 03:42 JST",
           "endDate": "2022/05/13 06:54 JST",
           "affectedServices": [
@@ -118,12 +118,12 @@
       "events": [
         {
           "usersAffected": "一部のユーザに影響します",
-          "epochStartDate": 1652500740000,
+          "epochStartDate": 1652489460000,
           "epochEndDate": null,
           "messageId": "2000001433",
           "statusType": "Issue",
-          "datePosted": "2022/05/14 13:10 JST",
-          "startDate": "2022/05/14 12:59 JST",
+          "datePosted": "2022/05/14 13:19 JST",
+          "startDate": "2022/05/14 09:51 JST",
           "endDate": null,
           "affectedServices": [
             "Apple Business Essentials",
@@ -313,7 +313,7 @@
           "epochEndDate": 1652392440000,
           "messageId": "2000001429",
           "statusType": "Issue",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/13 03:42 JST",
           "endDate": "2022/05/13 06:54 JST",
           "affectedServices": [
@@ -351,7 +351,7 @@
           "epochEndDate": 1652294280000,
           "messageId": "2000001426",
           "statusType": "Outage",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/12 02:05 JST",
           "endDate": "2022/05/12 03:38 JST",
           "affectedServices": [
@@ -373,7 +373,7 @@
           "epochEndDate": 1652294280000,
           "messageId": "2000001426",
           "statusType": "Outage",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/12 02:05 JST",
           "endDate": "2022/05/12 03:38 JST",
           "affectedServices": [
@@ -500,7 +500,7 @@
           "epochEndDate": 1652392440000,
           "messageId": "2000001429",
           "statusType": "Issue",
-          "datePosted": "2022/05/14 13:10 JST",
+          "datePosted": "2022/05/14 13:19 JST",
           "startDate": "2022/05/13 03:42 JST",
           "endDate": "2022/05/13 06:54 JST",
           "affectedServices": [
