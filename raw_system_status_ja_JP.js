6342f53bdda93fb23a8e4fda990bd8a7165b54fb
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-06-25 23:31:08 +0900

    diff commit 2022/06/25_23:31

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -1,6 +1,6 @@
 {
-  "drpost": false,
   "drMessage": null,
+  "drpost": false,
   "services": [
     {
       "redirectUrl": null,
@@ -53,7 +53,7 @@
           "epochEndDate": 1656198000000,
           "messageId": "2000001508",
           "statusType": "Maintenance",
-          "datePosted": "2022/06/25 23:28 JST",
+          "datePosted": "2022/06/25 23:30 JST",
           "startDate": "2022/06/25 22:00 JST",
           "endDate": "2022/06/26 08:00 JST",
           "affectedServices": null,
