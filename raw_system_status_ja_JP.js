da99fe5f85bfee83266efac05527fe94e74de9b9
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-09-26 17:06:28 +0900

    diff commit 2022/09/26_17:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -75,19 +75,7 @@
     {
       "redirectUrl": null,
       "events": [
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1663944120000,
-          "epochEndDate": 1663950660000,
-          "messageId": "2000001761",
-          "statusType": "Issue",
-          "datePosted": "2022/09/24 04:05 JST",
-          "startDate": "2022/09/23 23:42 JST",
-          "endDate": "2022/09/24 01:31 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
-        }
+
       ],
       "serviceName": "Apple Pay & ウォレット"
     },
