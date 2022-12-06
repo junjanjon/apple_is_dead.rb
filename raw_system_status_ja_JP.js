edf3fa13cb58b59004d192916b5dd2a4242ddc26
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2022-12-06 18:15:42 +0900

    diff commit 2022/12/06_18:15

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -280,29 +280,29 @@
       "events": [
         {
           "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1670219400000,
-          "epochEndDate": 1670229600000,
-          "messageId": "1000001589",
-          "statusType": "Issue",
-          "datePosted": "2022/12/06 14:32 JST",
-          "startDate": "2022/12/05 14:50 JST",
-          "endDate": "2022/12/05 17:40 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "このサービスを利用できなかった可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1670285400000,
           "epochEndDate": 1670292480000,
           "messageId": "2000001941",
           "statusType": "Issue",
-          "datePosted": "2022/12/06 14:32 JST",
+          "datePosted": "2022/12/06 18:00 JST",
           "startDate": "2022/12/06 09:10 JST",
           "endDate": "2022/12/06 11:08 JST",
           "affectedServices": null,
           "eventStatus": "resolved",
           "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
+        },
+        {
+          "usersAffected": "一部のユーザに影響しました",
+          "epochStartDate": 1670219400000,
+          "epochEndDate": 1670229600000,
+          "messageId": "1000001589",
+          "statusType": "Issue",
+          "datePosted": "2022/12/06 18:00 JST",
+          "startDate": "2022/12/05 14:50 JST",
+          "endDate": "2022/12/05 17:40 JST",
+          "affectedServices": null,
+          "eventStatus": "resolved",
+          "message": "このサービスを利用できなかった可能性があります。"
         }
       ],
       "serviceName": "iWorkの共同制作機能"
