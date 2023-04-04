ca9c120433deb221b734be9ff7ea157ba60d86e4
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2023-04-05 04:06:36 +0900

    diff commit 2023/04/05_04:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -426,17 +426,17 @@
       "redirectUrl": null,
       "events": [
         {
-          "usersAffected": "一部のユーザに影響します",
+          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1680577200000,
-          "epochEndDate": null,
+          "epochEndDate": 1680629460000,
           "messageId": "1000001964",
           "statusType": "Issue",
-          "datePosted": "2023/04/04 23:36 JST",
+          "datePosted": "2023/04/05 03:48 JST",
           "startDate": "2023/04/04 12:00 JST",
-          "endDate": null,
+          "endDate": "2023/04/05 02:31 JST",
           "affectedServices": null,
-          "eventStatus": "ongoing",
-          "message": "このサービスの速度が低下する、またはこのサービスを利用できない可能性があります。"
+          "eventStatus": "resolved",
+          "message": "このサービスの速度が低下した、またはこのサービスを利用できなかった可能性があります。"
         }
       ],
       "serviceName": "天気"
