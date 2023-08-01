2e62c733e03138c470c0bf918f3db219d88708e0
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2023-08-02 01:31:00 +0900

    diff commit 2023/08/02_01:31

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -73,7 +73,7 @@
           "epochEndDate": 1692651600000,
           "messageId": "1000002844",
           "statusType": "Maintenance",
-          "datePosted": "2023/08/01 23:15 JST",
+          "datePosted": "2023/08/02 01:30 JST",
           "startDate": "2023/08/21 23:45 JST",
           "endDate": "2023/08/22 06:00 JST",
           "affectedServices": null,
@@ -81,17 +81,17 @@
           "message": "メンテナンス中は、お客様がアメリカン・エキスプレス・インターナショナル, Inc.のカードをApple Payに追加することはできません。\n"
         },
         {
-          "usersAffected": "一部のユーザに影響します",
+          "usersAffected": "一部のユーザに影響しました",
           "epochStartDate": 1690899300000,
           "epochEndDate": 1690907400000,
           "messageId": "1000002843",
           "statusType": "Maintenance",
-          "datePosted": "2023/08/01 23:15 JST",
+          "datePosted": "2023/08/02 01:30 JST",
           "startDate": "2023/08/01 23:15 JST",
           "endDate": "2023/08/02 01:30 JST",
           "affectedServices": null,
-          "eventStatus": "ongoing",
-          "message": "メンテナンス中は、お客様がアメリカン・エキスプレス・インターナショナル, Inc.のカードをApple Payに追加できない可能性があります。"
+          "eventStatus": "completed",
+          "message": "メンテナンス中に、お客様はアメリカン・エキスプレス・インターナショナル, Inc.のカードをApple Payに追加できませんでした。"
         }
       ],
       "redirectUrl": null,
