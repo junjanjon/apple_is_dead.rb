5b692c9559228251846efe2e7ff2855c892d0157
hiroi+circleci <hiroi+circleci@users.noreply.github.com>
2023-11-17 18:06:30 +0900

    diff commit 2023/11/17_18:06

--- raw_system_status_ja_JP.js
+++ raw_system_status_ja_JP.js
@@ -73,22 +73,9 @@
           "epochEndDate": 1700097240000,
           "messageId": "1000003527",
           "statusType": "Issue",
-          "datePosted": "2023/11/17 11:00 JST",
+          "datePosted": "2023/11/17 18:00 JST",
           "startDate": "2023/11/16 09:42 JST",
           "endDate": "2023/11/16 10:14 JST",
-          "affectedServices": null,
-          "eventStatus": "resolved",
-          "message": "一部のユーザが、サービスをご利用になれなかった可能性があります。"
-        },
-        {
-          "usersAffected": "一部のユーザに影響しました",
-          "epochStartDate": 1700011500000,
-          "epochEndDate": 1700013420000,
-          "messageId": "1000003526",
-          "statusType": "Issue",
-          "datePosted": "2023/11/17 11:00 JST",
-          "startDate": "2023/11/15 10:25 JST",
-          "endDate": "2023/11/15 10:57 JST",
           "affectedServices": null,
           "eventStatus": "resolved",
           "message": "一部のユーザが、サービスをご利用になれなかった可能性があります。"
